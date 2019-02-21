2a1e937a14ac05ce42561297887f17297aa08471
Omkar Mote <omkarmote@Omkar-Mote.local>
2019-02-21 23:05:55 +0530

    Implemented move-grid-left function

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,13 +1,28 @@
 (ns twenty48.core
   (:gen-class))
 
+(def remove-zeros (partial remove zero?))
+
+(def apply-addition (partial map (partial apply +)))
+
+(def combinations (comp
+                          (partial mapcat (partial partition-all 2))
+                          (partial partition-by identity)))
+
+(def add-consecutive-equals (comp apply-addition combinations))
+
+(defn add-subordinate-zeros [row] (take 4 (concat row (repeat 0))))
+
+(def move-left (comp add-subordinate-zeros add-consecutive-equals remove-zeros))
+
+; -----------------------------------------------------------------------------------------------------------------------
+
+(def move-grid-left
+  "Moves an entire grid to the left"
+  (partial map move-left))
+
 (defn move-grid-right
   "Moves an entire grid to the right"
-  [grid]
-  grid)
-
-(defn move-grid-left
-  "Moves an entire grid to the left"
   [grid]
   grid)
 
@@ -20,3 +35,5 @@
   "Moves an entire grid up"
   [grid]
   grid)
+
+; -----------------------------------------------------------------------------------------------------------------------
